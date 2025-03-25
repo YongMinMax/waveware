ab78a0af30668864fb77a0220387489fb1ceaf2a
yen <yeahnk814@waveware.co.kr>
2025-03-25 17:06:02 +0900

    2025.03.25. KYE - mod _app.js
    - 헤더와 푸터 추가

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import "../styles/globals.css";
+import Header from "../layouts/header";
+import Footer from "../layouts/footer";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header/>
+      <Component {...pageProps} />
+      <Footer/>
+    </>
+  );
 }
