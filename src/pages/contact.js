ac4fa4c1125b91ca47e7c54ec186f1302934fb3b
yen <yeahnk814@waveware.co.kr>
2025-03-26 17:10:02 +0900

    2025.03.25. KYE - mod&add) pages
    - 각 페이지

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import MenuHeader from "../kwon/components/menuheader";
+
+export default function Contact() {
+    return (
+        <main className="min-h-screen flex flex-col items-center justify-center">
+
+            <MenuHeader title={"Contact"} description={"텍스트 텍스트/ 텍스트 텍스트 텍스트"}/>
+
+        </main>
+    );
+}
