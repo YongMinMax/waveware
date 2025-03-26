efca6f01e5f505de2bad3fa87f3cf49a31fc1043
yen <yeahnk814@waveware.co.kr>
2025-03-26 16:10:57 +0900

    2025.03.25. KYE - add) menuheader.js
    - 각 페이지 상단 컴포넌트

---
+++ src/kwon/components/menuheader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function MenuHeader({ title, description }) {
+  return (
+    <div className="flex flex-col w-[1400px] items-end gap-6 border-b-2 border-gray-200 pb-12">
+      <h1 className="text-primary text-xl">{title}</h1>
+      <div className="text-4xl font-semibold text-right">
+        {description?.split('/').map((text, index) => (
+          <p key={index}>{text}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
