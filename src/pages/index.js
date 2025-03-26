c688b5df957157307b8dc46ffb7cb5c4494f375b
yen <yeahnk814@waveware.co.kr>
2025-03-26 15:53:51 +0900

    2025.03.25. KYE - mod) pages/index.js
    - 히어로 섹션, 커리어 섹션 적용

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
+import HeroSection from "../kwon/pages/main/HeroSection";
+import Career from "../kwon/pages/main/career";
+
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <div className="container bg-white shadow p-8">
-        <h1 className="text-2xl font-bold">컨테이너 예시</h1>
-        <p>최대 너비가 1440px</p>
-      </div>
+    <main className="min-h-screen flex flex-col items-center justify-center">
+        <HeroSection/>
+        <Career/>
     </main>
   );
 }
