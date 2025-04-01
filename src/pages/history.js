b6a8c015d4198d71c2f28fb834dff498e1cdf0cd
yen <yeahnk814@waveware.co.kr>
2025-04-01 16:48:39 +0900

    2025.04.01. KYE - mod) header, history - etc

--- src/pages/history.js
+++ src/pages/history.js
@@ -4,7 +4,7 @@ export default function History() {
     return (
         <main className="min-h-screen flex flex-col items-center justify-center">
 
-            <MenuHeader title={"History"} description={"우리가 걸어온 길 어저고 저쩌고"}/>
+            <MenuHeader title={"History"} description={"기술로 써 내려온 성장의 기록/ 미래를 향한 우리의 발자취입니다."}/>
 
         </main>
     );
