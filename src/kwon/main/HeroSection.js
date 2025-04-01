2a7172b7ef966ba9f3ee715bf03787d44b7ba485
yen <yeahnk814@waveware.co.kr>
2025-04-01 10:10:09 +0900

    2025.04.01. KYE - mod) HeroSection.js - 화면비 , 애니메이션
    - 화면 꽉차게, 텍스트 애니메이션

--- src/kwon/main/HeroSection.js
+++ src/kwon/main/HeroSection.js
@@ -5,7 +5,7 @@ export default function HeroSection() {
     <section
       id="hero"
       className="relative w-full"
-      style={{ height: 'calc(100vh - 80px)', borderBottomRightRadius: '140px', overflow: 'hidden' }}
+      style={{ height: 'calc(100vh)', borderBottomRightRadius: '140px', overflow: 'hidden' }}
     >
       {/* 배경 비디오 */}
       <video
@@ -22,7 +22,7 @@ export default function HeroSection() {
       <div className="relative container h-full flex items-center text-white">
         <div className="flex gap-x-28 items-center">
           {/* 텍스트 그룹 */}
-          <div className="flex flex-col items-start pb-10">
+          <div className="flex flex-col items-start pb-10 animate-slide-up">
             <span className="text-[32px]">WE ARE</span>
             <h1 className="text-[62px] font-bold">WAVEWARE</h1>
           </div>
@@ -31,7 +31,7 @@ export default function HeroSection() {
           <div className="w-[400px] h-[10px] bg-white font-medium"></div>
 
           {/* 설명 텍스트 */}
-          <div className="text-[30px] pt-20">
+          <div className="text-[30px] pt-20 animate-slide-up">
             <p>메타데이터를 처리하고</p>
             <p>과학적 발전을 지원하며</p>
             <p>2D/3D 데이터 시각화를 수행합니다.</p>
