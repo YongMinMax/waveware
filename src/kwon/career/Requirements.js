7f1e93d1925bbc153f449bc1e61fcb46c2ef2bc9
yen <yeahnk814@waveware.co.kr>
2025-04-02 11:43:26 +0900

    2025.04.02. KYE - mod) page - career
    - 지원 역량 부분 수정

--- src/kwon/career/Requirements.js
+++ src/kwon/career/Requirements.js
@@ -2,10 +2,13 @@ import React from "react";
 
 export default function Requirements({requirements }) {
     return(
-        <div className="w-[400px] h-[500px] flex flex-col mt-12 px-4">
+        <div className="w-[400px] h-[600px] flex flex-col mt-12 px-4">
             <p className="text-primary text-xl">{requirements.eng}</p>
             <p className="text-3xl font-semibold pb-10">{requirements.kor}</p>
             <p className="text-gray-600 text-lg">{requirements.description}</p>
+            <div className="w-full h-full flex items-end justify-end mb-10">
+                <img src={requirements.img} alt="requirement" className="w-[330px]"/>
+            </div>
         </div>
     )
 }
--- src/pages/career.js
+++ src/pages/career.js
@@ -42,18 +42,21 @@ export default function Career() {
         kor: "성장하고자 하는 사람",
         description:
         "팀원을 소모품이 아닌 필수품으로 생각합니다. 함께 성장해 나갈 수 있는 환경을 만듭니다.",
+        img: "/img/1_color.png",
         },
     2: {
         eng: "Communication",
         kor: "열린 마음으로 듣는 사람",
         description:
         "서로의 아이디어를 존중하고 열린 마음으로 받아들이며, 적극적인 토론과 피드백을 통해 함께 성장합니다.",
+        img: "/img/4_color.png",
         },
     3: {
         eng: "Challenge",
         kor: "끊임없이 도전하는 사람",
         description:
         "목표를 설정하고 이를 위해 노력하는 사람을 환영합니다. 실패를 두려워하지 않고 도전하는 사람을 찾습니다.",
+        img: "/img/3_color.png",
     }
 }
 
@@ -91,7 +94,7 @@ export default function Career() {
             </div>
 
             <span className="w-[1400px] pb-4 text-5xl font-bold text-left">Who We Wants</span>
-            <section className="w-full h-[600px] bg-gray-100 flex justify-center">
+            <section className="w-full h-[700px] bg-gray-100 flex justify-center">
                 <div className="w-[1400px] h-full flex justify-between items-center">
                     <Requirements requirements={requirements[1]}/>
                     <div className="w-[1.5px] h-[80%] bg-gray-300"></div>
