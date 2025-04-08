0f59a493e2770c24f1722ac1be03506ed44a0646
yen <yeahnk814@waveware.co.kr>
2025-04-08 17:16:27 +0900

    2025.04.08. KYE - mod) career

--- src/pages/career.js
+++ src/pages/career.js
@@ -5,6 +5,7 @@ import Requirements from "../kwon/career/Requirements";
 import SectionTitle from "../kwon/components/section_title";
 import RecruitmentProcess from "../kwon/career/recruitment_process";
 import WhoWeWant from "../kwon/career/who_we_want";
+import Apply from "../kwon/career/Apply";
 
 export default function Career() {
 
@@ -21,7 +22,7 @@ export default function Career() {
 
             <WhoWeWant/>
 
-            {/*신규 입사자 교육 / 신입 사원 공개 채용 / 주차 비용 / */}
+            <Apply/>
 
         </main>
     );
