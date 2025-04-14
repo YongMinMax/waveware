2d2ad670fa2d543e580251efb738c3f192cd1f8a
yen <yeahnk814@waveware.co.kr>
2025-04-14 14:17:26 +0900

    2025.04.14. KYE - mod) section title
    - 모바일

--- src/kwon/components/section_title.js
+++ src/kwon/components/section_title.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function SectionTitle({ text }) {
     return (
         <div className="w-full flex justify-center">
-            <span className="w-[1400px] pt-32 pb-4 text-6xl font-bold text-left">{text}</span>
+            <span className="w-full mx-6 md:w-[1400px] pt-32 pb-4 text-3xl font-bold text-left md:text-6xl">{text}</span>
         </div>
     )
 }
